c47d86727878c532fab2d26e029a5ef3b133ffb5
Alberto Villalobos <alberto@albertovillalobos.me>
2015-08-09 23:29:57 -0700

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/blogplatform-dev'
+  },
+
+  seedDB: true
+};
